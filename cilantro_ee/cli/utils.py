db1eb7d964b24636cd5926d4ff9ff03ce356aa2d
tejas <tejas@lamden.io>
2020-03-24 21:51:09 -0700

    clean up - pkg verification added

--- cilantro_ee/cli/utils.py
+++ cilantro_ee/cli/utils.py
@@ -7,15 +7,18 @@ import cilantro_ee
 from checksumdir import dirhash
 from contracting.client import ContractingClient
 from cilantro_ee.storage.contract import BlockchainDriver
+from cilantro_ee.logger.base import get_logger
+
+log = get_logger('Cmd')
 
 
 def validate_ip(address):
     try:
         ip = ipaddress.ip_address(address)
-        print('%s is a correct IP%s address.' % (ip, ip.version))
+        log.info('%s is a correct IP%s address.' % (ip, ip.version))
         return ip
     except ValueError:
-        print('address/netmask is invalid: %s' % address)
+        log.error('address/netmask is invalid: %s' % address)
 
 
 def build_pepper(pkg_dir_path=os.environ.get('CIL_PATH')):
@@ -24,11 +27,13 @@ def build_pepper(pkg_dir_path=os.environ.get('CIL_PATH')):
         pkg_dir_path = '/Volumes/dev/lamden/cilantro-enterprise'
 
     pepper = dirhash(pkg_dir_path, 'sha256', excluded_extensions = ['pyc'])
-    print(pepper)
     return pepper
 
 
 def verify_cil_pkg(pkg_hash):
+    if pkg_hash is None:
+        return False
+
     current_pepper = build_pepper(pkg_dir_path = os.environ.get('CIL_PATH'))
 
     if current_pepper == pkg_hash:
@@ -42,6 +47,13 @@ def run(*args):
 
 
 def version_reboot():
+    driver = BlockchainDriver()
+    active_upgrade = driver.get_var(contract='upgrade', variable='upg_lock', mark=False)
+
+    if active_upgrade is True:
+        target_version = driver.get_var(contract='upgrade', variable='upg_pepper', mark=False)
+    else:
+        target_version = None
 
     try:
         path = os.environ.get('CIL_PATH')
@@ -53,10 +65,19 @@ def version_reboot():
 
         run("checkout", "-b", br)
     except OSError as err:
-        print("OS error: {0}".format(err))
+        log.error("OS error: {0}".format(err))
+        return
     except:
-        print("Unexpected error:", sys.exc_info())
-        raise
+        log.error("Unexpected error:", sys.exc_info())
+        return
+
+    result = verify_cil_pkg(target_version)
+
+    if result is False:
+        log.error("Failed to verify pepper {} on branch {}".format(target_version, rel))
+        return
+    else:
+        log.info("Pkg signature verified proceeding with reboot")
 
     # Find cil process
     PNAME = 'cil'
@@ -65,8 +86,6 @@ def version_reboot():
         if proc.name() == PNAME:
             print("{} : {} proc shutting down".format(proc.pid, proc.name()))
             proc.kill()
-
-
 
 
 def get_update_state():

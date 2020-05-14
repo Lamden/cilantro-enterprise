ed9b5dfaa19ea13f24e2a10c6e90772d5932a6c5
Stuart Farmer <stuart@lamden.io>
2020-05-14 12:00:10 -0700

    catchup functions tested

--- tests/unit/test_block_service.py
+++ tests/unit/test_block_service.py
@@ -8,6 +8,11 @@ import asyncio
 from unittest import TestCase
 
 
+async def stop_server(s, timeout):
+    await asyncio.sleep(timeout)
+    s.stop()
+
+
 class TestBlockService(TestCase):
     def setUp(self):
         self.ctx = zmq.asyncio.Context()
@@ -102,3 +107,104 @@ class TestBlockService(TestCase):
         res = self.loop.run_until_complete(self.b.process_message(msg))
 
         self.assertIsNone(res)
+
+
+class TestCatchupFunctions(TestCase):
+    def setUp(self):
+        self.ctx = zmq.asyncio.Context()
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+
+        self.b = masternode.BlockService(
+            blocks=storage.BlockStorage(),
+            driver=storage.StateDriver()
+        )
+
+        self.r = router.Router(
+            socket_id='tcp://127.0.0.1:18001',
+            ctx=self.ctx
+        )
+
+        self.r.add_service(base.BLOCK_SERVICE, self.b)
+
+    def tearDown(self):
+        self.ctx.destroy()
+        self.loop.close()
+        self.b.blocks.drop_collections()
+        self.b.driver.flush()
+
+    def test_get_latest_block_height(self):
+        self.b.driver.set_latest_block_num(1337)
+
+        async def send_msg():
+            res = await base.get_latest_block_height(
+                ip_string='tcp://127.0.0.1:18001',
+                ctx=self.ctx
+            )
+            return res
+
+        tasks = asyncio.gather(
+            self.r.serve(),
+            send_msg(),
+            stop_server(self.r, 0.2)
+        )
+
+        _, res, _ = self.loop.run_until_complete(tasks)
+
+        self.assertEqual(res, 1337)
+
+    def test_service_returns_block_for_number_if_exists(self):
+        block = {
+            'hash': (b'\x00' * 32).hex(),
+            'blockNum': 1337,
+            'previous': (b'\x00' * 32).hex(),
+            'subblocks': []
+        }
+
+        self.b.blocks.store_block(block)
+
+        async def send_msg():
+            res = await base.get_block(
+                block_num=1337,
+                ip_string='tcp://127.0.0.1:18001',
+                ctx=self.ctx
+            )
+            return res
+
+        tasks = asyncio.gather(
+            self.r.serve(),
+            send_msg(),
+            stop_server(self.r, 0.2)
+        )
+
+        _, res, _ = self.loop.run_until_complete(tasks)
+
+        self.assertEqual(res, block)
+
+    def test_service_returns_none_if_no_block_found(self):
+        block = {
+            'hash': (b'\x00' * 32).hex(),
+            'blockNum': 1337,
+            'previous': (b'\x00' * 32).hex(),
+            'subblocks': []
+        }
+
+        self.b.blocks.store_block(block)
+
+        async def send_msg():
+            res = await base.get_block(
+                block_num=7331,
+                ip_string='tcp://127.0.0.1:18001',
+                ctx=self.ctx
+            )
+            return res
+
+        tasks = asyncio.gather(
+            self.r.serve(),
+            send_msg(),
+            stop_server(self.r, 0.2)
+        )
+
+        _, res, _ = self.loop.run_until_complete(tasks)
+
+        self.assertDictEqual(res, router.OK)

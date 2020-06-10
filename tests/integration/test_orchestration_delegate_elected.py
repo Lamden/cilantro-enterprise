03223557a321f96ab8a4bdbacb1e9c3fd7c50d43
sanko61 <assem1@ya.ru>
2020-06-10 23:33:49 +0300

    Added test delegate elected and vouted out.

--- tests/integration/test_orchestration_delegate_elected.py
+++ tests/integration/test_orchestration_delegate_elected.py
@@ -221,15 +221,190 @@ class TestFullFlowWithMocks(TestCase):
 
             network.delegates.append(candidate_delegate)
 
-            # await network.make_and_push_tx(
-            #     wallet=stu,
-            #     contract='currency',
-            #     function='transfer',
-            #     kwargs={
-            #         'amount': 1,
-            #         'to': 'jeff'
-            #     }
-            # )
+            await network.make_and_push_tx(
+                wallet=stu,
+                contract='currency',
+                function='transfer',
+                kwargs={
+                    'amount': 1338,
+                    'to': candidate2.verifying_key
+                },
+            )
+            await asyncio.sleep(4)
+
+            self.assertEqual(network.get_var(
+                contract='currency',
+                variable='balances',
+                arguments=[candidate2.verifying_key]
+            ), 1338)
+
+            await asyncio.sleep(4)
+            await asyncio.sleep(4)
+
+        self.loop.run_until_complete(test())
+
+        network.stop()
+        delegates = network.get_var(contract='delegates', variable='S', arguments=['members'])
+        self.assertListEqual(
+            delegates,
+            [*[node.wallet.verifying_key for node in network.delegates]]  # , candidate.verifying_key
+        )
+        network.flush()
+
+
+    def test_vote_out_new_delegate(self):
+        network = mocks.MockNetwork(num_of_masternodes=2, num_of_delegates=2, ctx=self.ctx)
+
+        stu = Wallet()
+        candidate = Wallet()
+        candidate2= Wallet()
+
+        async def test():
+            await network.start()
+            network.refresh()
+
+            await network.make_and_push_tx(
+                wallet=mocks.TEST_FOUNDATION_WALLET,
+                contract='currency',
+                function='transfer',
+                kwargs={
+                    'amount': 1_000_000,
+                    'to': stu.verifying_key
+                }
+            )
+
+            await asyncio.sleep(1)
+
+            await network.make_and_push_tx(
+                wallet=mocks.TEST_FOUNDATION_WALLET,
+                contract='currency',
+                function='transfer',
+                kwargs={
+                    'amount': 1_000_000,
+                    'to': candidate.verifying_key
+                }
+            )
+            await asyncio.sleep(1)
+
+            await network.make_and_push_tx(
+                wallet=mocks.TEST_FOUNDATION_WALLET,
+                contract='currency',
+                function='transfer',
+                kwargs={
+                    'amount': 1_000_000,
+                    'to': network.delegates[0].wallet.verifying_key
+                }
+            )
+            await asyncio.sleep(1)
+
+            await network.make_and_push_tx(
+                wallet=mocks.TEST_FOUNDATION_WALLET,
+                contract='currency',
+                function='transfer',
+                kwargs={
+                    'amount': 1_000_000,
+                    'to': network.delegates[1].wallet.verifying_key
+                }
+            )
+
+            await asyncio.sleep(1)
+
+            await network.make_and_push_tx(
+                wallet=candidate,
+                contract='currency',
+                function='approve',
+                kwargs={
+                    'amount': 100_000,
+                    'to': 'elect_delegates'
+                }
+            )
+
+            await asyncio.sleep(1)
+
+            await network.make_and_push_tx(
+                wallet=candidate,
+                contract='elect_delegates',
+                function='register'
+            )
+
+            await asyncio.sleep(1)
+
+            await network.make_and_push_tx(
+                wallet=stu,
+                contract='currency',
+                function='approve',
+                kwargs={
+                    'amount': 100_000,
+                    'to': 'elect_delegates'
+                }
+            )
+
+            await asyncio.sleep(1)
+
+            await network.make_and_push_tx(
+                wallet=stu,
+                contract='elect_delegates',
+                function='vote_candidate',
+                kwargs={
+                    'address': candidate.verifying_key
+                }
+            )
+
+            await asyncio.sleep(1)
+
+            await network.make_and_push_tx(
+                wallet=network.delegates[0].wallet,
+                contract='election_house',
+                function='vote',
+                kwargs={
+                    'policy': 'delegates',
+                    'value': ('introduce_motion', 2)
+                }
+            )
+
+            await asyncio.sleep(1)
+
+            await network.make_and_push_tx(
+                wallet=network.delegates[0].wallet,
+                contract='election_house',
+                function='vote',
+                kwargs={
+                    'policy': 'delegates',
+                    'value': ('vote_on_motion', True)
+                }
+            )
+
+            await asyncio.sleep(1)
+
+            await network.make_and_push_tx(
+                wallet=network.delegates[1].wallet,
+                contract='election_house',
+                function='vote',
+                kwargs={
+                    'policy': 'delegates',
+                    'value': ('vote_on_motion', True)
+                }
+            )
+
+            await asyncio.sleep(4)
+
+            candidate_delegate = mocks.MockDelegate(
+                ctx=self.ctx,
+                index=999
+            )
+
+            # candidate_delegate.wallet = candidate
+            #
+            # constitution = deepcopy(network.constitution)
+            # bootnodes = deepcopy(network.bootnodes)
+            #
+            # constitution['delegates'].append(candidate.verifying_key)
+            #
+            # candidate_delegate.set_start_variables(bootnodes=bootnodes, constitution=constitution)
+            #
+            # await candidate_delegate.start()
+            #
+            # network.delegates.append(candidate_delegate)
 
             await network.make_and_push_tx(
                 wallet=stu,
@@ -249,12 +424,6 @@ class TestFullFlowWithMocks(TestCase):
             ), 1338)
 
             await asyncio.sleep(4)
-            # self.assertEqual(candidate_delegate.driver.get_var(
-            #     contract='currency',
-            #     variable='balances',
-            #     arguments=[candidate2.verifying_key]
-            # ), 1338)
-
             await asyncio.sleep(4)
 
         self.loop.run_until_complete(test())
@@ -263,9 +432,11 @@ class TestFullFlowWithMocks(TestCase):
         # delegates = network.get_var(contract='delegates', variable='S', arguments=['members'])
         # self.assertListEqual(
         #     delegates,
-        #     [*[node.wallet.verifying_key for node in network.delegates], candidate.verifying_key]
+        #     [*[node.wallet.verifying_key for node in network.delegates]]  # , candidate.verifying_key
         # )
+        #
+        self.assertFalse(
+            candidate.verifying_key in [node.wallet.verifying_key for node in network.delegates])
         # network.flush()
-
 
 

477db1ab5cca6a1ce7b1beae77fd04e1a6493a49
Stuart Farmer <stuart@lamden.io>
2020-06-15 11:43:51 -0700

    remove gath

--- cilantro_ee/nodes/delegate/delegate.py
+++ cilantro_ee/nodes/delegate/delegate.py
@@ -76,21 +76,6 @@ class WorkProcessor(router.Processor):
 
         self.todo.clear()
 
-    async def gather_transaction_batches(self, queue: dict, expected_batches: int, timeout=5):
-        # Wait until the queue is filled before starting timeout
-        while len(set(queue.keys())) == 0:
-            await asyncio.sleep(0)
-
-        # Now wait until the rest come in or the timeout is triggered
-        start = time.time()
-        while len(set(queue.keys())) < expected_batches and time.time() - start < timeout:
-            await asyncio.sleep(0)
-
-        work = deepcopy(list(queue.values()))
-        queue.clear()
-
-        return work
-
     async def accept_work(self, expected_batched, masters):
         self.accepting_work = True
         self.masters = masters

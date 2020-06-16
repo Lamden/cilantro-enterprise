9388a941336e731f36de97a60c11bdee71544130
Stuart Farmer <stuart@lamden.io>
2020-06-16 13:22:20 -0700

    token asserts

--- cilantro_ee/contracts/genesis/currency.s.py
+++ cilantro_ee/contracts/genesis/currency.s.py
@@ -6,7 +6,10 @@ def seed(vk: str):
 
 @export
 def transfer(amount: float, to: str):
+    assert amount > 0, 'Cannot send negative balances!'
+
     sender = ctx.caller
+
     assert balances[sender] >= amount, 'Not enough coins to send!'
 
     balances[sender] -= amount
@@ -26,12 +29,16 @@ def allowance(owner: str, spender: str):
 
 @export
 def approve(amount: float, to: str):
+    assert amount > 0, 'Cannot send negative balances!'
+
     sender = ctx.caller
     balances[sender, to] += amount
     return balances[sender, to]
 
 @export
 def transfer_from(amount: float, to: str, main_account: str):
+    assert amount > 0, 'Cannot send negative balances!'
+
     sender = ctx.caller
 
     assert balances[main_account, sender] >= amount, 'Not enough coins approved to send! You have {} and are trying to spend {}'\

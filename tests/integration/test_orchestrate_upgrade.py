82fd74160636b9ddec4659dac82bb39d067f99df
Stuart Farmer <stuart@lamden.io>
2020-07-02 17:31:32 -0700

    new pepper

--- tests/integration/test_orchestrate_upgrade.py
+++ tests/integration/test_orchestrate_upgrade.py
@@ -272,8 +272,7 @@ class TestUpgradeOrchestration(unittest.TestCase):
         current_branch = get_version()
         current_contracting_branch = get_version(os.path.join(os.path.dirname(contracting.__file__), '..'))
 
-        cil_path = os.path.dirname(cilantro_ee.__file__)
-        pepper = build_pepper(cil_path)
+        pepper = '351767476eb79434d60e47d3e349c7417a55912fe1fd8d1c53b5cc2f9944d173'
 
         network = mocks.MockNetwork(num_of_masternodes=2, num_of_delegates=3, ctx=self.ctx)
         network.flush()

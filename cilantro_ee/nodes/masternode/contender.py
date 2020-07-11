9e640ef71446a8b84fbf604f8eac687f7d6f8c70
Stuart Farmer <stuart@lamden.io>
2020-07-11 11:39:12 -0700

    50

--- cilantro_ee/nodes/masternode/contender.py
+++ cilantro_ee/nodes/masternode/contender.py
@@ -110,7 +110,7 @@ class PotentialSolution:
 
 
 class SubBlockContender:
-    def __init__(self, input_hash, index, total_contacts, required_consensus=0.66, adequate_consensus=0.51):
+    def __init__(self, input_hash, index, total_contacts, required_consensus=0.66, adequate_consensus=0.50):
         self.input_hash = input_hash
         self.index = index
 
